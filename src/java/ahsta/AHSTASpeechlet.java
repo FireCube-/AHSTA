d9c248db7a595fa8c40417338b54dd52f06b5df9
Gwion ap Rheinallt <Gwion ap Rheinallt>
2017-03-04 19:15:17 +0000

    Speechlet possibly finished2

--- src/java/ahsta/AHSTASpeechlet.java
+++ src/java/ahsta/AHSTASpeechlet.java
@@ -20,19 +20,44 @@ public class AHSTASpeechlet implements Speechlet {
 	@Override
 	public void onSessionStarted(SessionStartedRequest request, Session session) throws SpeechletException {
 		log.info("onSessionStarted requestId={}, sessionId={}", request.getRequestId(), session.getSessionId());
-
+initializeComponents();
 	}
 
 	@Override
+	//onlaunch is run when the skill is opened without an intent
+	//not sure whether to use this or an intent to launch the program - keeping it here for now
 	public SpeechletResponse onLaunch(LaunchRequest request, Session session) throws SpeechletException {
 		log.info("onLaunch requestId={}, sessionId={}", request.getRequestId(), session.getSessionId());
-		return null;
-	}
+		return AHSTAGameManager.getLaunchResponse(request, session);
+}
 
 	@Override
 	public SpeechletResponse onIntent(IntentRequest request, Session session) throws SpeechletException {
 		log.info("onIntent requestId={}, sessionId={}", request.getRequestId(), session.getSessionId());
-		return null;
+		initializeComponents();
+
+		Intent intent = request.getIntent();
+		if ("Option1Intent".equals(intent.getName())) {
+			return AHSTAGameManager.getOption1IntentResponse(intent, session);
+
+		} else if ("Option2Intent".equals(intent.getName())) {
+			return AHSTAGameManager.getOption2IntentResponse(intent, session);
+
+		} else if ("Option3Intent".equals(intent.getName())) {
+			return AHSTAGameManager.getOption3IntentResponse(intent, session);
+
+		} else if ("Option4Intent".equals(intent.getName())) {
+			return AHSTAGameManager.getOption4IntentResponse(intent, session);
+
+		} else if ("NewGameIntent".equals(intent.getName())) {
+			return AHSTAGameManager.getNewGameIntentResponse(intent, session);
+
+		} else if ("AMAZON.StopIntent".equals(intent.getName())) {
+			return AHSTAGameManager.getExitIntentResponse(intent, session);
+
+		} else {
+			throw new IllegalArgumentException("Unrecognized intent: " + intent.getName());
+		}
 	}
 
 	@Override
