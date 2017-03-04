d9765726bbcab3f5d77ba40e3354a31b253833ba
Balint Gyevnar <bgyevnar@8afbf85b.st-andrews.ac.uk>
2017-03-04 18:52:22 +0000

    Added dependencies

--- src/java/ahsta/AHSTAGame.java
+++ src/java/ahsta/AHSTAGame.java
@@ -1,5 +1,45 @@
 package ahsta;
 
-public class AHSTAGame {
+import java.io.Serializable;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import com.amazon.speech.speechlet.Session;
+
+/**
+ * Represents a score keeper game.
+ */
+public final class AHSTAGame {
+    private Session session;
+    private AHSTAGameData gameData;
+
+    private AHSTAGame() {
+    }
+
+    
+    public static AHSTAGame newInstance(Session session, AHSTAGameData gameData) {
+    	AHSTAGame game = new AHSTAGame();
+        game.setSession(session);
+        game.setGameData(gameData);
+        return game;
+    }
+
+    protected void setSession(Session session) {
+        this.session = session;
+    }
+
+    protected Session getSession() {
+        return session;
+    }
+
+    protected AHSTAGameData getGameData() {
+        return gameData;
+    }
+
+    protected void setGameData(AHSTAGameData gameData) {
+        this.gameData = gameData;
+    }
 
 }
