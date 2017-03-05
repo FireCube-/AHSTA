a023081434590b745e7a60dd286d6495615f14da
Murto <murto@airmail.cc>
2017-03-05 03:36:58 +0000

    Move parsing functions to XMLParser and add custom XML exception

--- src/main/textadventure/Game.java
+++ src/main/textadventure/Game.java
@@ -1,12 +1,7 @@
 package textadventure;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 
 public class Game {
-
+	
 	private Location location;
 	private Inventory inventory;
 
@@ -17,112 +12,6 @@ public class Game {
 	boolean doAction(String action) {
 		//Does the action specified as the parameter
 		return true;
-	}
-
-	boolean loadLocation(String name) throws FileNotFoundException {
-		//Loads the location from xml and stores it in the location member variable.
-		FileReader fr = new FileReader(System.getProperty("user.dir") + "/" + name + ".xml");
-		BufferedReader br = new BufferedReader(fr);
-		String check = Parser.getTag(br);
-		if (!check.equals("location"))
-			return false;
-		String tag = Parser.getTag(br);
-		String locName = "";
-		String locDesc = "";
-		ArrayList<Entity> locEntities = new ArrayList<>();
-		ArrayList<Connection> locConnections = new ArrayList<>();
-		while (!tag.equals("/location")) {
-			switch(tag) {
-				case "name":
-					locName = Parser.getContent(br);
-					if (!Parser.getTag(br).equals("/name"))
-						return false;
-					break;
-				case "desc":
-					locDesc = Parser.getContent(br);
-					if (!Parser.getTag(br).equals("/desc"))
-						return false;
-					break;
-				case "entity":
-					locEntities.add(this.loadEntity(br));
-					break;
-				case "connection":
-					locConnections.add(this.loadConnection(br));
-					break;
-				default:
-					return false;
-			}
-			tag = Parser.getTag(br);
-		}
-		this.location = new Location(locName, locConnections, locDesc, locEntities);
-		return true;
-	}
-
-	private Entity loadEntity(BufferedReader br) {
-		String tag = Parser.getTag(br);
-		String eName = "";
-		String eDesc = "";
-		HashSet<String> eProps = new HashSet<>();
-		while (!tag.equals("/entity")) {
-			switch(tag) {
-				case "name":
-					eName = Parser.getContent(br);
-					if (!Parser.getTag(br).equals("/name"))
-						return new Entity("ERROR", "INVALID ENTITY NAME");
-					break;
-				case "desc":
-					eDesc = Parser.getContent(br);
-					if (!Parser.getTag(br).equals("/desc"))
-						return new Entity("ERROR", "INVALID ENTITY DESCRIPTION");
-					break;
-				case "property":
-					eProps.add(Parser.getContent(br));
-					if (!Parser.getTag(br).equals("/property"))
-						return new Entity("ERROR", "INVALID ENTITY PROPERTY");
-					break;
-				default:
-					return new Entity("ERROR", "INVALID ENTITY XML");
-		
-			}
-			tag = Parser.getTag(br);
-		}
-		return new Entity(eName, eDesc);
-	}
-	
-	private Connection loadConnection(BufferedReader br) {
-		String tag = Parser.getTag(br);
-		String cName = "";
-		String cDesc = "";
-		String cDestination = "";
-		while (!tag.equals("/connection")) {
-			switch(tag) {
-				case "name":
-					cName = Parser.getContent(br);
-					if (!Parser.getTag(br).equals("/name"))
-						return new Connection("ERROR", "INVALID CONNECTION NAME");
-					break;
-				case "desc":
-					cName = Parser.getContent(br);
-					if (!Parser.getTag(br).equals("/desc"))
-						return new Connection("ERROR", "INVALID CONNECTION DESCRIPTION");
-					break;
-				case "destination":
-					cDestination = Parser.getContent(br);
-					if (!Parser.getTag(br).equals("/destination"))
-						return new Connection("ERROR", "INVALID CONNECTION DESTINATION");
-					break;
-				default:
-					return new Connection("ERROR", "INVALID CONNECTION XML");
-		
-			}
-			tag = Parser.getTag(br);
-		}
-		return new Connection(cName, cDestination, cDesc);
-	}
-
-	void printLocation() {
-		System.out.println("name: " + this.location.getName());
-		System.out.println("desc: " + this.location.getDesc());
 	}
 
 }
--- src/main/textadventure/Test.java
+++ src/main/textadventure/Test.java
@@ -4,12 +4,7 @@ import java.io.FileNotFoundException;
 
 public class Test {
 	public static void main(String[] args) throws FileNotFoundException {
-		Game game = new Game();
-		if (game.loadLocation("test-location"))
-			game.printLocation();
-		else
-			System.out.println("Returned false!");
-			
+		//Tests go here
 	}
 
 }
